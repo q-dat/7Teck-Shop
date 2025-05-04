61303def78ad9a06f03e955981b969bd9fbb552b
q-dat <quocdatstore79@gmail.com>
2025-05-04 10:38:09 +0700

    init source

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   return (
     <html lang="en" data-theme="mytheme">
       <head></head>
