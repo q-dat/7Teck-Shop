6da14744edd86bcc403f87df438c0c5d59557ba3
q-dat <quocdatstore79@gmail.com>
2025-05-06 12:16:48 +0700

    chore: assets/index.ts

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,5 +1,5 @@
 export const images = {
-  Logo:'/images/Logo.webp',
+  Logo:'/images/Logo.jpg',
   LogoZalo:'/images/LogoZalo.webp',
   Popup:'/images/Popup.webp',
   bannerBackground:'/images/bannerBackground.webp',
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   icons: {
-    icon: '/favicon.png',
+    icon: './favicon.png',
   },
 };
 
