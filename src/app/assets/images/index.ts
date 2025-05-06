013a8e0f9466e52aa8218788aa14a648142c7db8
q-dat <quocdatstore79@gmail.com>
2025-05-06 11:19:10 +0700

    chore: assets/index.ts

--- src/app/assets/images/index.ts
+++ src/app/assets/images/index.ts
@@ -1,8 +1,8 @@
-import Logo from '@/assets/images/Logo.webp';
-import LogoZalo from '@/assets/images/LogoZalo.webp';
-import Popup from '@/assets/images/Popup.webp';
-import bannerBackground from '@/assets/images/bannerBackground.webp';
-import bannerForeground from '@/assets/images/bannerForeground.webp';
+import Logo from '/src/app/assets/images/Logo.webp';
+import LogoZalo from '/src/app/assets/images/LogoZalo.webp';
+import Popup from '/src/app/assets/images/Popup.webp';
+import bannerBackground from '/src/app/assets/images/bannerBackground.webp';
+import bannerForeground from '/src/app/assets/images/bannerForeground.webp';
 export const images = {
   Logo,
   LogoZalo,
