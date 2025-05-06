631536c24bf6beb36ba05e359fdbbc7f8e79eb0b
q-dat <quocdatstore79@gmail.com>
2025-05-06 07:37:22 +0700

    chore: assets folder

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,15 +1,15 @@
-import BannerMobile from './images/BannerMobile.webp';
-import BannerTablet from './images/BannerTablet.webp';
-import BannerDesktop from './images/BannerDesktop.webp';
-import Logo from './images/Logo.png';
-import LogoFacebook from './images/LogoFacebook.webp';
-import LogoEmail from './images/LogoEmail.webp';
-import LogoZalo from './images/LogoZalo.webp';
-import bgBlog from './images/bgBlog.webp';
-import bgFixed from './images/bgFixed.webp';
-import bannerBackground from './images/bannerBackground.webp';
-import bannerForeground from './images/bannerForeground.webp';
-import Popup from './images/Popup.webp';
+import BannerMobile from 'src/app/assets/images/BannerMobile.webp';
+import BannerTablet from 'src/app/assets/images/BannerTablet.webp';
+import BannerDesktop from 'src/app/assets/images/BannerDesktop.webp';
+import Logo from 'src/app/assets/images/Logo.png';
+import LogoFacebook from 'src/app/assets/images/LogoFacebook.webp';
+import LogoEmail from 'src/app/assets/images/LogoEmail.webp';
+import LogoZalo from 'src/app/assets/images/LogoZalo.webp';
+import bgBlog from 'src/app/assets/images/bgBlog.webp';
+import bgFixed from 'src/app/assets/images/bgFixed.webp';
+import bannerBackground from 'src/app/assets/images/bannerBackground.webp';
+import bannerForeground from 'src/app/assets/images/bannerForeground.webp';
+import Popup from 'src/app/assets/images/Popup.webp';
 
 export const images = {
   BannerMobile,
