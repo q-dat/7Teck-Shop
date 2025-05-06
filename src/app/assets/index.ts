dee98485cc28edb555b84226b124afd0c6ac0990
q-dat <quocdatstore79@gmail.com>
2025-05-06 09:51:38 +0700

    chore: assets folder

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -2,7 +2,7 @@ export const images = {
   BannerMobile: '/images/BannerMobile.webp',
   BannerTablet: '/images/BannerTablet.webp',
   BannerDesktop: '/images/BannerDesktop.webp',
-  Logo: '/images/Logo.png',
+  Logo: '/images/Logo.webp',
   LogoFacebook: '/images/LogoFacebook.webp',
   LogoEmail: '/images/LogoEmail.webp',
   LogoZalo: '/images/LogoZalo.webp',
