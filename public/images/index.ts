b422f736170ba17e74486b53c7baee17eb8eed1b
q-dat <quocdatstore79@gmail.com>
2025-05-06 12:25:56 +0700

    chore: assets/index.ts

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,7 +1,7 @@
 export const images = {
   Logo:'/images/Logo.jpg',
   LogoZalo:'/images/LogoZalo.webp',
-  Popup:'/images/Popup.webp',
+  Popup:'/images/Popup.jpg',
   bannerBackground:'/images/bannerBackground.webp',
   bannerForeground:'/images/bannerForeground.webp'
 };
