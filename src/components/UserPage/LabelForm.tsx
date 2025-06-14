39bf9b57685d8d0ffdf242315d850fd8b97c006c
ㅤㅤㅤĐiểu Quốc Đạtㅤㅤㅤ <168713454+q-dat@users.noreply.github.com>
2025-06-15 00:30:28 +0700

    Delete src/components/UserPage directory

--- src/components/UserPage/LabelForm.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import React from 'react';
-
-const LabelForm: React.FC<{ title: string }> = ({ title }) => {
-  return <label className="text-sm">{title}</label>;
-};
-
-export default LabelForm;
