6093ec48714c5dbd9aac53fca912207ec00d4a30
q-dat <quocdatstore79@gmail.com>
2025-07-13 20:51:10 +0700

    chore: ProductPlaceholders

--- src/components/userPage/ProductPlaceholders.tsx
+++ src/components/userPage/ProductPlaceholders.tsx
@@ -5,7 +5,7 @@ const ProductPlaceholders = ({ count = 0 }: { count?: number }) => {
   return (
     <>
       {Array.from({ length: count }).map((_, index) => (
-        <div key={index} className="w-[195px] p-2">
+        <div key={index} className="w-[185px]">
           <div className="animate-pulse space-y-2">
             <div className="h-[200px] w-full rounded-md bg-primary/20" />
             <div className="h-2 w-3/4 bg-primary/20" />
