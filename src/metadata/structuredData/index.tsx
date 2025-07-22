27fef996e9c6950d753987ffefb6c6221e93197f
q-dat <quocdatstore79@gmail.com>
2025-07-22 17:48:19 +0700

    chore: StructuredData

--- src/metadata/structuredData/index.tsx
+++ src/metadata/structuredData/index.tsx
@@ -1,8 +1,14 @@
 'use client';
 import React from 'react';
 
+type StructuredDataType = {
+  '@context': string;
+  '@type': string;
+  [key: string]: unknown;
+};
+
 type StructuredDataProps = {
-  data: Record<string, any>;
+  data: StructuredDataType;
 };
 
 export const StructuredData: React.FC<StructuredDataProps> = ({ data }) => {
