f9b677306851cf986f36491d8f4339f72003ae39
q-dat <quocdatstore79@gmail.com>
2025-08-13 18:15:59 +0700

    feat: ClientUsedProductsPageUI in UserPage

--- src/app/thiet-bi-da-qua-su-dung/layout.tsx
+++ src/app/thiet-bi-da-qua-su-dung/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-primary-white pb-[60px]">{children}</div>;
+  return <div className="bg-primary-white">{children}</div>;
 }
